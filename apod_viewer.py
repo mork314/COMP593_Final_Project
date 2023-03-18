a7c3cae959c10c5d63d31e550f4fbffc32a63608
ABC <pufflicorne@gmail.com>
2023-03-18 17:56:15 -0400

    we are getting there but man this is tough

--- apod_viewer.py
+++ apod_viewer.py
@@ -28,11 +28,25 @@ maxdate = date.today()
 
 
 
-def open_calendar():
+def open_calendar():  
+
+    top = Toplevel(root)
+
+    cal = Calendar(top, selectmode = 'day', mindate=mindate, maxdate=maxdate)
+    cal.pack(pady=20)
+
+    button = Button(top, text="Select This Date", command=lambda: get_selected_date(cal, top))
+    button.pack()
+
+    top.wait_window(top)
+
+    date_selected = cal.selection_get()
+        
+    return date_selected
+
+def get_selected_date(calendar, top):
     
-    def get_selected_date():
-    
-        selected_date = cal.selection_get()
+        selected_date = calendar.selection_get()
         
         date_str = selected_date.strftime("%Y-%m-%d")
         
@@ -40,55 +54,85 @@ def open_calendar():
 
         print(date_str)
 
-        return date_str
-
-    top = Toplevel(root)
-
-    cal = Calendar(top, selectmode = 'day', mindate=mindate, maxdate=maxdate)
-    cal.pack(pady=20)
-
-    button = Button(top, text="Select This Date", command=get_selected_date)
-    button.pack()
 
 def get_date_and_image():
 
-    open_calendar()
+    apod_date = open_calendar()
 
     apod_id = apod_desktop.add_apod_to_cache(apod_date)
 
-    print(apod_id)
+    apod_info = apod_desktop.get_apod_info(apod_id)
+
+    image_path = apod_info['file_path']
+
+    image_info = apod_info['explanation']
+
+    display_image_and_explanation(image_path, image_info)
 
 button = Button(root, text = "Open the Calendar to Select a Date", command=get_date_and_image)
 
 button.pack(pady=20)
 
+def display_image_and_explanation(image_path, image_info):
+    
+    
+
+
+    img_to_display = Image.open(image_path)
+
+    #Scale image to a max height of 300, width of 550
+
+    img_to_display.thumbnail((550, 300))
+
+    tk_image = ImageTk.PhotoImage(img_to_display)
+
+    img_label = Label(image=tk_image)
+    
+    #This is a fix I found online to stop my image from being 'Garbage collected' - I guess the program decides tk_image can get thrown away
+    # once we move far enough along ? Not sure
+
+    img_label.image = tk_image
+
+    img_label.place(relx = 0.5, rely = 0.25, anchor = 'center')
+
+    explanation_label = Label(root, text = image_info, wraplength = 550, bg = "deep pink", fg = "yellow")
+
+    explanation_label.place(relx = 0.5, rely = 0.85, anchor = 'center')  
+    
+
+    #scales font size w/ amount of text
+
+    font_size = 5
+
+    while explanation_label.winfo_reqwidth() < 550:
+        font_size += 1
+        explanation_label.config(font=("Arial", font_size))
+
+    #Sets font as comic sans
+
+    font_tuple = ("Comic Sans MS", font_size, "bold")
+
+    explanation_label.configure(font = font_tuple)
+
+ 
 
 
 
-#OK THIS DISPLAYS AN IMAGE OF YOUR CHOICE BIG MONEY
-current_apod = Image.open(r'C:\Users\markn\OneDrive\Documents\GitHub\COMP593_Final_Project\image_cache_directory\Earth_or_Mars.jpg')
-test = ImageTk.PhotoImage(current_apod)
 
-img_label = Label(image=test)
-desc_label = Label(root, text = 'DESCRIPTION', bg = "deep pink", fg = "yellow")
 
-img_label.pack()
 
-#NOT REALLY SURE HOW THIS CENTERING STUFF WORKS - THIS CODE PLACES THE ANCHOR IN THE UPPER LEFT CORNER OF THE SCREEN
 
-desc_label.place(relx = 0.5, rely = 0.925, anchor = 'center')
+
+
 
 #select date from calendar --> translate date into proper format for apod_desktop functions
 # use apod_desktop functions to get that image, import title, description, file location to this script
 # display image w/ title and description.
 
-#alternatively, just select image from cache by title.
 
-#Sets font as comic sans
 
-font_tuple = ("Comic Sans MS", 30, "bold")
 
-desc_label.configure(font = font_tuple)
+
 
 
 root.mainloop()
