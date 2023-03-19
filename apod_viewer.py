d4d53471e23f347a5d5219ec219b2707747e2d36
ABC <pufflicorne@gmail.com>
2023-03-19 13:53:16 -0400

    Trying to make window resize - not dynamic yet :(

--- apod_viewer.py
+++ apod_viewer.py
@@ -17,11 +17,16 @@ apod_desktop.init_apod_cache(script_dir)
 #Creates window
 root = Tk()
 #Sets size of window
-root.geometry('600x400')
+window_width = 1200
+
+window_height = 800
+
+root.geometry(f'{window_width}x{window_height}')
 #Gives window a title
 root.title("Astronomy Picture of the Day Viewer")
 #Creates calendar and adds to window
 
+#These variables will be used later to limit the range of the calendar
 mindate = date(1995, 6, 16)
 maxdate = date.today()
 
@@ -30,13 +35,18 @@ maxdate = date.today()
 
 def open_calendar():  
 
+    #Opens a 'top level' window
     top = Toplevel(root)
 
+    #creates a calendar widget in the top level window
     cal = Calendar(top, selectmode = 'day', mindate=mindate, maxdate=maxdate)
     cal.pack(pady=20)
 
-    button = Button(top, text="Select This Date", command=lambda: get_selected_date(cal, top))
+    #creates and places a button that closes the calendar window when pressed
+    button = Button(top, text="Select This Date", command=lambda: top.destroy())
     button.pack()
+
+    #waits until the top window is closed to execute the next line
 
     top.wait_window(top)
 
@@ -44,18 +54,11 @@ def open_calendar():
         
     return date_selected
 
-def get_selected_date(calendar, top):
-    
-        selected_date = calendar.selection_get()
-        
-        date_str = selected_date.strftime("%Y-%m-%d")
-        
-        top.destroy()
-
-        print(date_str)
-
 
 def get_date_and_image():
+
+    for widget in root.winfo_children():
+        widget.destroy()
 
     apod_date = open_calendar()
 
@@ -69,20 +72,29 @@ def get_date_and_image():
 
     display_image_and_explanation(image_path, image_info)
 
-button = Button(root, text = "Open the Calendar to Select a Date", command=get_date_and_image)
+    create_calendar_button()
 
-button.pack(pady=20)
+
+def create_calendar_button():
+    
+    button = Button(root, text = "Open the Calendar to Select a Date", command=get_date_and_image)
+    
+    button.pack(pady=20)
+
 
 def display_image_and_explanation(image_path, image_info):
     
-    
+    global window_width, window_height
 
+    window_width = root.winfo_width()
+
+    window_height = root.winfo_height()
 
     img_to_display = Image.open(image_path)
 
-    #Scale image to a max height of 300, width of 550
+    #Scale image to window size
 
-    img_to_display.thumbnail((550, 300))
+    img_to_display.thumbnail((0.9 * window_width, 0.75 * window_height))
 
     tk_image = ImageTk.PhotoImage(img_to_display)
 
@@ -95,16 +107,17 @@ def display_image_and_explanation(image_path, image_info):
 
     img_label.place(relx = 0.5, rely = 0.25, anchor = 'center')
 
-    explanation_label = Label(root, text = image_info, wraplength = 550, bg = "deep pink", fg = "yellow")
+    explanation_label = Label(root, text = image_info, wraplength = 0.95 * window_width, bg = "deep pink", fg = "yellow")
 
     explanation_label.place(relx = 0.5, rely = 0.85, anchor = 'center')  
     
+    
 
-    #scales font size w/ amount of text
+    #scales font size w/ amount of text & window size - weird things happened when it was a float value so we're just doing this :D
 
-    font_size = 5
+    font_size = int(12 * (window_height / 800))
 
-    while explanation_label.winfo_reqwidth() < 550:
+    while explanation_label.winfo_reqwidth() < 0.95 * window_width:
         font_size += 1
         explanation_label.config(font=("Arial", font_size))
 
@@ -117,21 +130,13 @@ def display_image_and_explanation(image_path, image_info):
  
 
 
-
-
-
-
-
-
-
-
 #select date from calendar --> translate date into proper format for apod_desktop functions
 # use apod_desktop functions to get that image, import title, description, file location to this script
 # display image w/ title and description.
 
 
 
-
+create_calendar_button()
 
 
 
